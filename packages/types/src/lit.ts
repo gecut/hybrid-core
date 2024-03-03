435115162b3b3f2707a1e4d4bb66a1bfd3f3042c
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-03-03 13:10:31 +0330

    feat(types/lit): `RenderResult`

--- packages/types/src/lit.ts
+++ packages/types/src/lit.ts
@@ -1,4 +1,4 @@
 import type { SingleOrArray } from './type-helper.js';
 import type { TemplateResult, noChange, nothing } from 'lit';
 
-export type RenderResult = SingleOrArray<TemplateResult> | typeof nothing | typeof noChange;
+export type RenderResult = SingleOrArray<TemplateResult> | typeof nothing | typeof noChange | unknown;
