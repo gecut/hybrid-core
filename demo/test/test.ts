5f1ac6bf665b7eda344cd99be4d6d9e65c7fddc7
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-05-04 16:18:34 +0330

    feat(demo): complete

--- demo/test/test.ts
+++ demo/test/test.ts
@@ -1,8 +1,30 @@
 import {GecutQueue} from '@gecut/utilities/queue.js';
-import {untilMS} from '@gecut/utilities/wait/wait.js';
+import {untilEvent, untilMS} from '@gecut/utilities/wait/wait.js';
 
-const queue = new GecutQueue('demo', 500);
+const queue = new GecutQueue('demo', 1500);
 
-queue.push(untilMS(5000), 'ms1');
+const f1 = queue.push(untilMS(5000), 'f1');
+const f2 = queue.push(untilEvent(document.body, 'dblclick'), 'f2');
+let timer = 0;
 
-console.log('fuck')
+setInterval(() => {
+  console.clear();
+
+  console.log(timer);
+
+  console.log({
+    id: f1.id,
+    isRunning: queue.isRunning(f1.id),
+    isFinished: queue.isFinished(f1.id),
+    value: queue.getValue(f1.id),
+  });
+
+  console.log({
+    id: f2.id,
+    isRunning: queue.isRunning(f2.id),
+    isFinished: queue.isFinished(f2.id),
+    value: queue.getValue(f2.id),
+  });
+
+  timer++;
+}, 1000);
