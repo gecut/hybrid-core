81453410e3868d4971abcce7041e6846c866e09b
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-05-04 15:41:11 +0330

    feat(demo): init

--- demo/test/test.ts
+++ demo/test/test.ts
@@ -1,1 +1,8 @@
-console.log('hello');
+import {GecutQueue} from '@gecut/utilities/queue.js';
+import {untilMS} from '@gecut/utilities/wait/wait.js';
+
+const queue = new GecutQueue('demo', 500);
+
+queue.push(untilMS(5000), 'ms1');
+
+console.log('fuck')
