9bcfca2dc618024a0ab260ab4c8a3c24360c29bd
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2024-07-04 16:29:56 +0330

    fix(signal): path

--- packages/signal/src/libraries/simple-signal.ts
+++ packages/signal/src/libraries/simple-signal.ts
@@ -10,13 +10,13 @@ export class SimpleSignal<T> extends Signal<T> {
    * @param {T} newValue - The new value to notify the subscribers with.
    */
   notify(newValue: T): void {
-    this.__$log.methodArgs?.('notify', {newValue});
+    this.logger.methodArgs?.('notify', {newValue});
 
     this.__$notify(newValue);
   }
 
   get nextValue(): Promise<T> {
-    this.__$log.method?.('nextValue');
+    this.logger.method?.('nextValue');
 
     return this.__$untilNewNotify();
   }
